652e397b6137af87c9ad51f5b14f00fac902f377
Sunghwan-DS <jeonsung02@gmail.com>
2023-07-28 14:04:14 +0900

    Create TFT Guide Project

---
+++ src/main/java/jsh/tftguide/TftGuideApplication.java
@@ -0,0 +1,13 @@
+package jsh.tftguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TftGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jsh/tftguide/TftGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package jsh.tftguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
